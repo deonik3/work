791b084170ffd883576420180effdd88110ad08b
deonik3 <kis.sergey2006@gmail.com>
2024-10-16 11:07:48 +0300

    Create matrix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+int** createMatrix(size_t M, size_t N
+void deleteTable(int** makeMatrix, size_t M, size_t N);
 int main(){
   size_t M = 0, N = 0;
+  int ** matrix = createMatrix(M,N)
   std::cin >> M >> N;
+  std:;cout << M << "" << N << "\n";
+  deleteTable(matrix, M, N);
 }
+
